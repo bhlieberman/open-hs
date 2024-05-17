4b93841999bc7e32d737a8352d492507ebce30b4
Ben Lieberman <bhlieberman93@gmail.com>
2024-05-17 08:33:30 -0600

    formatting and filtering

--- src/Code.hs
+++ src/Code.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
+
 module Code where
-    import Data.Aeson
-    import Data.Aeson.TH
-    import GHC.Generics ( Generic )
 
+import Data.Aeson
+import Data.Aeson.TH
+import GHC.Generics (Generic)
 
-    data Code = Code {
-        uuid :: String,
-        code :: String,
-        code_type :: String,
-        url :: Maybe String,
-        code_system :: String,
-        references :: Maybe [String]
-    } deriving (Generic, Show)
+data Code = Code
+  { uuid :: String,
+    code :: String,
+    code_type :: String,
+    url :: Maybe String,
+    code_system :: String,
+    references :: Maybe [String]
+  }
+  deriving (Generic, Show)
 
-    $(deriveJSON defaultOptions {fieldLabelModifier = \x -> if x == "code_type" then "type" else x} ''Code)
+$(deriveJSON defaultOptions {fieldLabelModifier = \x -> if x == "code_type" then "type" else x} ''Code)
--- src/Substances.hs
+++ src/Substances.hs
@@ -1,49 +1,65 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Substances where
-    import qualified Control.Exception as E
-    import Data.Aeson
-    import Data.ByteString
-    import GHC.Generics ( Generic )
-    import Network.HTTP.Simple
 
-    import qualified Code as C
+import qualified Code as C
+import qualified Control.Exception as E
+import Data.Aeson
+import qualified Data.ByteString as BS hiding (concatMap, filter)
+import qualified Data.List.NonEmpty as NE
+import GHC.Generics (Generic)
+import Network.HTTP.Simple
 
-    data Result = Result {
-        codes :: [C.Code],
-        substance_class :: String,
-        names :: [Object],
-        references :: [Object],
-        definition_type :: String,
-        moieties :: [Object],
-        definition_level :: String,
-        uuid :: String,
-        version :: String,
-        structure :: Object,
-        unii :: String
-    } deriving (Generic, Show)
+data Result = Result
+  { codes :: [C.Code],
+    substance_class :: String,
+    names :: [Object],
+    references :: [Object],
+    definition_type :: String,
+    moieties :: [Object],
+    definition_level :: String,
+    uuid :: String,
+    version :: String,
+    structure :: Object,
+    unii :: String
+  }
+  deriving (Generic, Show)
 
-    instance FromJSON Substances.Result
+instance FromJSON Substances.Result
 
-    data Substance = Substance {
-        meta :: Object,
-        results :: [Substances.Result]
-    } deriving (Generic, Show)
+data Substance = Substance
+  { meta :: Object,
+    results :: NE.NonEmpty Substances.Result
+  }
+  deriving (Generic, Show)
 
-    instance FromJSON Substance
+instance FromJSON Substance
 
-    getSubstancesJson :: IO (Response ByteString) 
-    getSubstancesJson = httpBS "https://api.fda.gov/other/substance.json?limit=1"
+getSubstancesJson :: IO (Response BS.ByteString)
+getSubstancesJson = httpBS "https://api.fda.gov/other/substance.json?limit=1"
 
-    getResults :: IO ()
-    getResults = do
-        E.bracket resp onError $
-            \body -> do
-                let json_ = eitherDecodeStrict body :: Either String Substance
-                case json_ of
-                    (Right j) -> print $ results j
-                    (Left err) -> putStrLn ("Error " <> err)
-        where
-            json = getSubstancesJson
-            resp = getResponseBody <$> json
-            onError = const $ getResponseStatusCode <$> json
+filterCasCodes :: Substance -> [C.Code]
+filterCasCodes sub =
+  let results_ :: NE.NonEmpty Substances.Result
+      results_ = results sub
+      codes_ :: [C.Code]
+      codes_ = codes $ NE.head results_
+      filt :: C.Code -> Bool
+      filt c =
+        let sys = C.code_system c
+         in sys == "CAS"
+   in filter filt codes_
+
+getResults :: IO ()
+getResults = do
+  E.bracket resp onError $
+    \body -> do
+      let json_ = eitherDecodeStrict body :: Either String Substance
+      case json_ of
+        (Right j) -> print $ filterCasCodes j
+        (Left err) -> putStrLn ("Error " <> err)
+  where
+    json = getSubstancesJson
+    resp = getResponseBody <$> json
+    onError = const $ getResponseStatusCode <$> json
